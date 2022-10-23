bff41aa3036d199f7b40da72c8b78ac7e008add2
Gabriel <gabriel.bur@outlook.com>
2022-10-23 18:21:05 +0100

    Adding QueryParams + PagedList

---
+++ src/Api/Helpers/PagedList.cs
@@ -0,0 +1,27 @@
+﻿namespace Api.Helpers
+{
+    public class PagedList<T>: List<T>
+    {
+        public int TotalResults { get; private set; }
+        public int CurrentPage { get; private set; }  
+        public int PageSize { get; private set; }
+
+        public PagedList(List<T> items, int total, int pageSize, int pageNumber) : base(items)
+        {
+            TotalResults = total;
+            PageSize = pageSize;
+            CurrentPage = pageNumber;
+        }
+
+        public static PagedList<T> BuildPageList(IQueryable<T> source, int pageSize, int pageNumber)
+        {
+            var count = source.Count();
+            var items = source
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            return new PagedList<T>(items, count, pageSize, pageNumber);
+        }
+    }
+}
---
+++ src/Api/Helpers/QueryParams.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+
+namespace Api.Helpers
+{
+    public class QueryParams
+    {
+        private const int MaxPagesize = 100;
+
+        private int _pageNumber;
+        public int PageNumber
+        {
+            get { return _pageNumber < 1 ? 1 : _pageNumber; }
+            set { _pageNumber = value; }
+        }
+
+        private int _pageSize;
+        public int PageSize
+        {
+            get { return _pageSize < 1 ? 1 : _pageSize; }
+            set { _pageSize = value > 100 ? MaxPagesize : value; }
+        }
+
+        public string? Sort { get; set; }
+
+        public static ValueTask<QueryParams?> BindAsync(
+            HttpContext context, ParameterInfo parameter)
+        {
+            const string sortKey = "sort";
+            const string pageNumberKey = "pageNumber";
+            const string PageSizeKey = "pageSize";
+
+            int.TryParse(context.Request.Query[pageNumberKey], out var pageNumber);
+            int.TryParse(context.Request.Query[PageSizeKey], out var pageSize);
+
+            var result = new QueryParams()
+            {
+                PageNumber = pageNumber,
+                PageSize = pageSize,
+                Sort = context.Request.Query[sortKey],
+            };
+
+            return ValueTask.FromResult<QueryParams?>(result);
+        }
+    }
+}
